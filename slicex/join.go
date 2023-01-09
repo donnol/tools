9e03e53c0b5fc6ea6c4589523effdf4500caab24
donnol <donnol@outlook.com>
2023-01-09 10:45:10 +0800

    slicex: refactor variable

--- slicex/join.go
+++ slicex/join.go
@@ -3,43 +3,43 @@ package slicex
 import "github.com/samber/lo"
 
 // Join nested loop，no limit about condition
-func Join[J, K, R any](
-	left []J,
-	right []K,
-	match func(J, K) bool,
-	mapper func(J, K) R,
+func Join[LE, RE, R any](
+	left []LE,
+	right []RE,
+	match func(lele LE, rele RE) bool,
+	mapper func(lele LE, rele RE) R,
 ) []R {
-	var r = make([]R, 0, len(left))
+	var result = make([]R, 0, len(left))
 
-	for _, j := range left {
-		for _, k := range right {
-			if !match(j, k) {
+	for _, leftEle := range left {
+		for _, rightEle := range right {
+			if !match(leftEle, rightEle) {
 				continue
 			}
-			r = append(r, mapper(j, k))
+			result = append(result, mapper(leftEle, rightEle))
 		}
 	}
 
-	return r
+	return result
 }
 
 // JoinByKey hash join, must be '=' condition
 func JoinByKey[K comparable, LE, RE, R any](
 	left []LE,
 	right []RE,
-	lk func(item LE) K,
-	rk func(item RE) K,
-	mapper func(LE, RE) R,
+	leftKey func(item LE) K,
+	rightKey func(item RE) K,
+	mapper func(lele LE, rele RE) R,
 ) []R {
-	var r = make([]R, 0, len(left))
+	var result = make([]R, 0, len(left))
 
-	rm := lo.KeyBy(right, rk)
+	rightMap := lo.KeyBy(right, rightKey)
 
-	for _, j := range left {
-		k := lk(j)
-		re := rm[k]
-		r = append(r, mapper(j, re))
+	for _, leftEle := range left {
+		exchangeKey := leftKey(leftEle)
+		rightEle := rightMap[exchangeKey]
+		result = append(result, mapper(leftEle, rightEle))
 	}
 
-	return r
+	return result
 }
--- slicex/join_test.go
+++ slicex/join_test.go
@@ -1,8 +1,11 @@
 package slicex
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+
+	"github.com/samber/lo"
 )
 
 type User struct {
@@ -21,23 +24,28 @@ type BookWithUser struct {
 	UserName string
 }
 
-func UserBookMatcher(j Book, k User) bool {
-	return j.Author == k.Id
+func UserBookMatcher(book Book, user User) bool {
+	return book.Author == user.Id
 }
 
-func TestJoin(t *testing.T) {
-	r := Join([]Book{
+var (
+	users = []User{
+		{Id: 1, Name: "jd"},
+		{Id: 2, Name: "jc"},
+	}
+	books = []Book{
 		{Id: 1, Title: "hello", Author: 1},
 		{Id: 2, Title: "world", Author: 1},
 		{Id: 3, Title: "good", Author: 2},
 		{Id: 4, Title: "job", Author: 2},
-	}, []User{
-		{Id: 1, Name: "jd"},
-		{Id: 2, Name: "jc"},
-	}, UserBookMatcher, func(j Book, k User) BookWithUser {
+	}
+)
+
+func TestJoin(t *testing.T) {
+	r := Join(books, users, UserBookMatcher, func(book Book, user User) BookWithUser {
 		return BookWithUser{
-			Book:     j,
-			UserName: k.Name,
+			Book:     book,
+			UserName: user.Name,
 		}
 	})
 	want := []BookWithUser{
@@ -52,22 +60,38 @@ func TestJoin(t *testing.T) {
 }
 
 func TestJoinByKey(t *testing.T) {
-	r := JoinByKey([]Book{
-		{Id: 1, Title: "hello", Author: 1},
-		{Id: 2, Title: "world", Author: 1},
-		{Id: 3, Title: "good", Author: 2},
-		{Id: 4, Title: "job", Author: 2},
-	}, []User{
-		{Id: 1, Name: "jd"},
-		{Id: 2, Name: "jc"},
-	}, func(item Book) uint64 {
+	r := JoinByKey(books, users, func(item Book) uint64 {
 		return item.Author
 	}, func(item User) uint64 {
 		return item.Id
-	}, func(j Book, k User) BookWithUser {
+	}, func(book Book, user User) BookWithUser {
 		return BookWithUser{
-			Book:     j,
-			UserName: k.Name,
+			Book:     book,
+			UserName: user.Name,
+		}
+	})
+	want := []BookWithUser{
+		{Book{1, "hello", 1}, "jd"},
+		{Book{2, "world", 1}, "jd"},
+		{Book{3, "good", 2}, "jc"},
+		{Book{4, "job", 2}, "jc"},
+	}
+	if !reflect.DeepEqual(r, want) {
+		t.Errorf("bad case, %+v != %+v", r, want)
+	}
+}
+
+func TestJoinByKey_CompositeKey(t *testing.T) {
+	type CompositeKey string
+
+	r := JoinByKey(books, users, func(item Book) CompositeKey {
+		return CompositeKey(fmt.Sprintf("%d|%d", item.Author, item.Author))
+	}, func(item User) CompositeKey {
+		return CompositeKey(fmt.Sprintf("%d|%d", item.Id, item.Id))
+	}, func(book Book, user User) BookWithUser {
+		return BookWithUser{
+			Book:     book,
+			UserName: user.Name,
 		}
 	})
 	want := []BookWithUser{
@@ -83,102 +107,64 @@ func TestJoinByKey(t *testing.T) {
 
 func BenchmarkJoin(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Join([]Book{
-			{Id: 1, Title: "hello", Author: 1},
-			{Id: 2, Title: "world", Author: 1},
-			{Id: 3, Title: "good", Author: 2},
-			{Id: 4, Title: "job", Author: 2},
-		}, []User{
-			{Id: 1, Name: "jd"},
-			{Id: 2, Name: "jc"},
-		}, UserBookMatcher, func(j Book, k User) BookWithUser {
+		Join(books, users, UserBookMatcher, func(book Book, user User) BookWithUser {
 			return BookWithUser{
-				Book:     j,
-				UserName: k.Name,
+				Book:     book,
+				UserName: user.Name,
 			}
 		})
 	}
 }
 
 func BenchmarkJoin100X(b *testing.B) {
-	users := []User{
-		{Id: 1, Name: "jd"},
-		{Id: 2, Name: "jc"},
-	}
-	books := []Book{
-		{Id: 1, Title: "hello", Author: 1},
-		{Id: 2, Title: "world", Author: 1},
-		{Id: 3, Title: "good", Author: 2},
-		{Id: 4, Title: "job", Author: 2},
-	}
-	for i := 0; i < 100; i++ {
-		users = append(users, User{
-			Id: 2, Name: "jc",
-		})
-		books = append(books, Book{
-			Id: 4, Title: "job", Author: 2,
-		})
-	}
+	users := lo.RepeatBy(100, func(index int) User {
+		return users[1]
+	})
+	books := lo.RepeatBy(100, func(index int) Book {
+		return books[3]
+	})
 
 	for i := 0; i < b.N; i++ {
-		Join(books, users, UserBookMatcher, func(j Book, k User) BookWithUser {
+		Join(books, users, UserBookMatcher, func(book Book, user User) BookWithUser {
 			return BookWithUser{
-				Book:     j,
-				UserName: k.Name,
+				Book:     book,
+				UserName: user.Name,
 			}
 		})
 	}
 }
 
 func BenchmarkJoinByKey(b *testing.B) {
-	JoinByKey([]Book{
-		{Id: 1, Title: "hello", Author: 1},
-		{Id: 2, Title: "world", Author: 1},
-		{Id: 3, Title: "good", Author: 2},
-		{Id: 4, Title: "job", Author: 2},
-	}, []User{
-		{Id: 1, Name: "jd"},
-		{Id: 2, Name: "jc"},
-	}, func(item Book) uint64 {
-		return item.Author
-	}, func(item User) uint64 {
-		return item.Id
-	}, func(j Book, k User) BookWithUser {
-		return BookWithUser{
-			Book:     j,
-			UserName: k.Name,
-		}
-	})
-}
-
-func BenchmarkJoinByKey100X(b *testing.B) {
-	users := []User{
-		{Id: 1, Name: "jd"},
-		{Id: 2, Name: "jc"},
-	}
-	books := []Book{
-		{Id: 1, Title: "hello", Author: 1},
-		{Id: 2, Title: "world", Author: 1},
-		{Id: 3, Title: "good", Author: 2},
-		{Id: 4, Title: "job", Author: 2},
-	}
-	for i := 0; i < 1_000_000; i++ {
-		users = append(users, User{
-			Id: 2, Name: "jc",
-		})
-		books = append(books, Book{
-			Id: 4, Title: "job", Author: 2,
-		})
-	}
-
 	JoinByKey(books, users, func(item Book) uint64 {
 		return item.Author
 	}, func(item User) uint64 {
 		return item.Id
-	}, func(j Book, k User) BookWithUser {
+	}, func(book Book, user User) BookWithUser {
 		return BookWithUser{
-			Book:     j,
-			UserName: k.Name,
+			Book:     book,
+			UserName: user.Name,
+		}
+	})
+}
+
+var (
+	usersX1M = lo.RepeatBy(1_000_000, func(index int) User {
+		return users[1]
+	})
+	booksX1M = lo.RepeatBy(1_000_000, func(index int) Book {
+		return books[3]
+	})
+)
+
+func BenchmarkJoinByKey100X(b *testing.B) {
+	JoinByKey(booksX1M, usersX1M, func(item Book) uint64 {
+		return item.Author
+	}, func(item User) uint64 {
+		return item.Id
+	}, func(book Book, user User) BookWithUser {
+		return BookWithUser{
+			Book:     book,
+			UserName: user.Name,
 		}
 	})
 }
